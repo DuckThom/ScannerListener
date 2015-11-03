6b22c505f8c6211cd20356bfc33994a8355d26b2
Luna Moonfang <thomas.wiringa@gmail.com>
2015-11-03 17:15:00 +0100

    Add Product class

---
+++ ScannerListener/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerListener
+{
+    class Product
+    {
+        private int number;
+        private int qty;
+        private string name;
+        private string location;
+
+        public Product(int number, int qty, string name, string location)
+        {
+            this.number = number;
+            this.qty = qty;
+            this.name = name;
+            this.location = location;
+        }
+
+        public int getNumber()
+        {
+            return number;
+        }
+
+        public int getQty()
+        {
+            return qty;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getLocation()
+        {
+            return location;
+        }
+    }
+}
